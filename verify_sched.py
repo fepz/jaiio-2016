cec9a18c13a7b2d837f4c535c3d10d3cca80fefe
fepz <franpaez@gmail.com>
2016-05-22 00:28:01 -0300

    Correct indentation.

--- verify_sched.py
+++ verify_sched.py
@@ -9,8 +9,8 @@ def main():
 
     # verify that the files exists
     if not os.path.isfile(dump_file):
-         print("{0}: file not found.".format(dump_file))
-         sys.exit(1)
+        print("{0}: file not found.".format(dump_file))
+        sys.exit(1)
 
     with open(dump_file, "rb") as infile:
         data = pickle.load(infile)
@@ -35,27 +35,16 @@ def main():
                 # Finalization time for the instances
                 mbed_samples_end = task_group.values[0][6:][1::2]
 
-                schedulable = 0
-
                 for x in range(len(mbed_samples_end)):
                     abs_deadline = (x + 1) * task_group.values[0][4]
                     if abs_deadline < mbed_samples_end[x]:
                         print("Task {0} is not schedulable for RTS {1}, FU {2}: {3} / {4}".format(task, rts, fu, mbed_samples_end[x], abs_deadline))
-                        schedulable = 1
-                        if rts_sched == 0:
-                            rts_sched = 1
+                        rts_sched = 1
                         break
-
-                #results[fu][schedulable] += 1
-                results2.append((fu, rts, task, schedulable))
 
             results[fu][rts_sched] += 1
 
     print(results)
-
-    #df2 = pd.DataFrame(results2, columns=["fu", "rts", "task", "schedulable"])
-    #with open("n5sched.dump", "wb") as infile:
-    #    pickle.dump(df2, infile)
 
 
 if __name__ == '__main__':
