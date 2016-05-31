7b95511fcfa28f6f0e4c24b45c139bab84bf91fb
fepz <franpaez@gmail.com>
2016-05-31 19:30:28 -0300

    Remove comments.

--- generate_results.py
+++ generate_results.py
@@ -75,6 +75,45 @@ def graph1(data, fus):
     plt.close(fig)
 
 
+def analyze_c_t2(data):
+    from matplotlib.backends.backend_pdf import PdfPages
+    with PdfPages('test5ct.pdf') as pdf:
+
+        # Group by fu
+        for fu, fu_group in data.groupby(["fu"]):
+            print("fu", fu)
+
+            # Group by task
+            for task, task_group in fu_group.groupby(["task"]):
+                period_l = []
+                wcet_l = []
+
+                fig, ax = plt.subplots(1, 1)
+                ax.margins(0.5, 0.5)
+                ax.set_xlabel('wcet')
+                ax.set_ylabel('t')
+
+                # Now group by rts
+                for rts, rts_group in task_group.groupby(["rts"]):
+                    # period
+                    period = rts_group.iloc[0]["t"]
+                    wcet = rts_group.iloc[0]["c"]
+
+                    if (wcet / period) <= ((fu * 30.0) / 100.0):
+                        period_l.append(period)
+                        wcet_l.append(wcet)
+
+                ax.set_xlim([0, np.max(wcet_l)])
+                ax.set_ylim([0, np.max(period_l)])
+
+                title = "Task {0}, FU {1}".format(task, fu)
+                plt.title(title)
+                ax.scatter(wcet_l, period_l)
+
+                pdf.savefig()  # saves the current figure into a pdf page
+                plt.close(fig)
+
+
 def analyze_c_t(data):
     from matplotlib.backends.backend_pdf import PdfPages
     with PdfPages('test5ct.pdf') as pdf:
@@ -104,10 +143,6 @@ def analyze_c_t(data):
                     
                 ax.set_xlim([0,np.max(wcet_l)])
                 ax.set_ylim([0,np.max(period_l)])
-
-                #print("task", task)
-                #print(np.mean(wcet_l), np.max(wcet_l), np.min(wcet_l), np.mean(period_l), np.max(period_l), np.min(period_l))
-                #print(np.max(wcet_l), period_l[np.argmax(wcet_l)])  # supuestamente nos tiene que dar el periodo de esta tarea
 
                 title = "Task {0}, FU {1}".format(task, fu)
                 plt.title(title)
